3f65cffdf22cabc060646e38c7218528f0f0c37d
Daniel Rodríguez <danielcarlosrodriguez@gmail.com>
2023-07-22 09:07:08 -0300

    ajustes de diseño - casette

--- src/components/DelSol.jsx
+++ src/components/DelSol.jsx
@@ -1,14 +1,90 @@
+import { useRef, useState } from "react";
 import H5AudioPlayer from "react-h5-audio-player";
 import "react-h5-audio-player/lib/styles.css";
 
-const DelSol = ({ streamUrl }) => {
+const DelSol = () => {
+  const [emisora, setEmisora] = useState("DelSol")
+
+  const [urlRadio, setUrlRadio] = useState(
+    "https://delsol-2.nty.uy/stream/1/?type=http&nocache=2780&dbl=0"
+  );
+
+  const videoRef = useRef(null);
+  
+
+  const handlePlay = () => {
+    videoRef.current.play();
+    console.log("Reproduciendo...");
+  };
+
+  const handlePause = () => {
+    videoRef.current.pause();
+    console.log("Pausado...");
+  };
+
+  const handleEmisora = (url, emisora) => {
+    setUrlRadio(url);
+    setEmisora(emisora);
+  };
+
+
+
   return (
-    <H5AudioPlayer
-      //autoPlay
-      src={streamUrl}
-      onPlay={(e) => console.log("Reproduciendo...")}
-      // Puedes agregar otros eventos aquí, como onPause, onEnded, etc.
-    />
+    <div className="container mt-5 text-center">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h1 className="text-white">{emisora}</h1>
+          <button
+            type="button"
+            className="btn btn-violet me-2"
+            onClick={() =>
+              handleEmisora(
+                "https://delsol-2.nty.uy/stream/1/?type=http&nocache=2780&dbl=0",
+                "DelSol"
+              )
+            }
+          >
+            Del Sol
+          </button>
+
+          <button
+            type="button"
+            className="btn btn-violet me-2"
+            onClick={() =>
+              handleEmisora(
+                "https://streaming2.hostingmontevideo.com:7003/stream?type=http&nocache=6078",
+                "M24"
+              )
+            }
+          >
+            M24
+          </button>
+        </div>
+
+        <div className="col-md-6 offset-md-3 ">
+          <div className="player mt-2 mx-4">
+            <H5AudioPlayer
+              autoPlay
+              src={urlRadio}
+              onPlay={handlePlay}
+              onPause={handlePause}
+            />
+          </div>
+        </div>
+        <div className="col-md-6 offset-md-3">
+          <div className="casette mt-2">
+            <video
+              ref={videoRef}
+              src="/casette.mp4"
+              controls
+              style={{ maxWidth: "100%", marginTop: "15px" }}
+            >
+              Your browser does not support the video tag.
+            </video>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import DelSol from "./components/DelSol";
-import M24Radio from "./components/M24Radio";
-
-const streamUrl =
-  "https://delsol-2.nty.uy/stream/1/?type=http&nocache=2780&dbl=0";
-
-const streamUrlM24 =
-  "https://streaming2.hostingmontevideo.com:7003/stream?type=http&nocache=6078"
+import "./css/index.css"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <div className="App">
-      <h1>Del Sol</h1>
-      <DelSol streamUrl={streamUrl} />
-      <h1>M24</h1>
-      <M24Radio streamUrl={streamUrlM24} />
+      <DelSol />
     </div>
   </React.StrictMode>
 );
-
-
-
